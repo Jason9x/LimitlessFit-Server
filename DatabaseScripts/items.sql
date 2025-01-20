1f920eadbfc6e299c281225f0ea5c98d5608804a
Jason Olimpio <olimpiojason@gmail.com>
2025-01-20 02:20:17 +0100

    Update items.sql

--- DatabaseScripts/items.sql
+++ DatabaseScripts/items.sql
@@ -8,3 +8,13 @@ create table items
     price           decimal(10, 2) not null
 );
 
+INSERT INTO items (id, image_url, name_key, description_key, price) VALUES (1, '/assets/images/items/ez_barbell.png', 'ez_barbell_name', 'ez_barbell_description', 31.99);
+INSERT INTO items (id, image_url, name_key, description_key, price) VALUES (2, '/assets/images/items/dumbbells_set.png', 'dumbbells_set_name', 'dumbbells_set_description', 25.99);
+INSERT INTO items (id, image_url, name_key, description_key, price) VALUES (3, '/assets/images/items/kettlebell.png', 'kettlebell_name', 'kettlebell_description', 27.99);
+INSERT INTO items (id, image_url, name_key, description_key, price) VALUES (4, '/assets/images/items/triceps_row.png', 'triceps_row_name', 'triceps_row_description', 11.99);
+INSERT INTO items (id, image_url, name_key, description_key, price) VALUES (5, '/assets/images/items/angle_bar.png', 'angle_bar_name', 'angle_bar_description', 29.99);
+INSERT INTO items (id, image_url, name_key, description_key, price) VALUES (6, '/assets/images/items/lat_machine_bar.png', 'lat_machine_bar_name', 'lat_machine_bar_description', 33.99);
+INSERT INTO items (id, image_url, name_key, description_key, price) VALUES (7, '/assets/images/items/pull_up_bar.png', 'pull_up_bar_name', 'pull_up_bar_description', 49.99);
+INSERT INTO items (id, image_url, name_key, description_key, price) VALUES (8, '/assets/images/items/jumping_rope.png', 'jumping_rope_name', 'jumping_rope_description', 5.99);
+INSERT INTO items (id, image_url, name_key, description_key, price) VALUES (9, '/assets/images/items/weight_disc.png', 'weight_disc_name', 'weight_disc_description', 6.99);
+INSERT INTO items (id, image_url, name_key, description_key, price) VALUES (10, '/assets/images/items/creatine.png', 'creatine_name', 'creatine_description', 4.99);
