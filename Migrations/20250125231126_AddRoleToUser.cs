74ebb0f3924c8615c45b3398dbf5a37042092603
Jason Olimpio <olimpiojason@gmail.com>
2025-01-26 00:32:13 +0100

    Add migrations

---
+++ Migrations/20250125231126_AddRoleToUser.cs
@@ -0,0 +1,69 @@
+﻿using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace LimitlessFit.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddRoleToUser : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<int>(
+                name: "role_id",
+                table: "users",
+                type: "int",
+                nullable: false,
+                defaultValue: 0);
+
+            migrationBuilder.CreateTable(
+                name: "role",
+                columns: table => new
+                {
+                    id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("MySql:ValueGenerationStrategy", MySqlValueGenerationStrategy.IdentityColumn),
+                    name = table.Column<string>(type: "varchar(50)", maxLength: 50, nullable: false)
+                        .Annotation("MySql:CharSet", "utf8mb4")
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("pk_role", x => x.id);
+                })
+                .Annotation("MySql:CharSet", "utf8mb4");
+
+            migrationBuilder.CreateIndex(
+                name: "ix_users_role_id",
+                table: "users",
+                column: "role_id");
+
+            migrationBuilder.AddForeignKey(
+                name: "fk_users_role_role_id",
+                table: "users",
+                column: "role_id",
+                principalTable: "role",
+                principalColumn: "id",
+                onDelete: ReferentialAction.Cascade);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "fk_users_role_role_id",
+                table: "users");
+
+            migrationBuilder.DropTable(
+                name: "role");
+
+            migrationBuilder.DropIndex(
+                name: "ix_users_role_id",
+                table: "users");
+
+            migrationBuilder.DropColumn(
+                name: "role_id",
+                table: "users");
+        }
+    }
+}
---
+++ Migrations/20250125231501_AddRolesTable.cs
@@ -0,0 +1,68 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace LimitlessFit.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddRolesTable : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "fk_users_role_role_id",
+                table: "users");
+
+            migrationBuilder.DropPrimaryKey(
+                name: "pk_role",
+                table: "role");
+
+            migrationBuilder.RenameTable(
+                name: "role",
+                newName: "roles");
+
+            migrationBuilder.AddPrimaryKey(
+                name: "pk_roles",
+                table: "roles",
+                column: "id");
+
+            migrationBuilder.AddForeignKey(
+                name: "fk_users_roles_role_id",
+                table: "users",
+                column: "role_id",
+                principalTable: "roles",
+                principalColumn: "id",
+                onDelete: ReferentialAction.Cascade);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "fk_users_roles_role_id",
+                table: "users");
+
+            migrationBuilder.DropPrimaryKey(
+                name: "pk_roles",
+                table: "roles");
+
+            migrationBuilder.RenameTable(
+                name: "roles",
+                newName: "role");
+
+            migrationBuilder.AddPrimaryKey(
+                name: "pk_role",
+                table: "role",
+                column: "id");
+
+            migrationBuilder.AddForeignKey(
+                name: "fk_users_role_role_id",
+                table: "users",
+                column: "role_id",
+                principalTable: "role",
+                principalColumn: "id",
+                onDelete: ReferentialAction.Cascade);
+        }
+    }
+}
