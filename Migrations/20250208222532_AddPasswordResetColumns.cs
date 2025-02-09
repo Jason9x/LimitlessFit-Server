ffe12ccef9a02a2d186c1aed274491995f69e854
Jason Olimpio <olimpiojason@gmail.com>
2025-02-09 04:48:45 +0100

    Add migrations

---
+++ Migrations/20250208222532_AddPasswordResetColumns.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace LimitlessFit.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddPasswordResetColumns : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "password_reset_token",
+                table: "users",
+                type: "varchar(255)",
+                maxLength: 255,
+                nullable: true)
+                .Annotation("MySql:CharSet", "utf8mb4");
+
+            migrationBuilder.AddColumn<DateTime>(
+                name: "password_reset_token_expiry",
+                table: "users",
+                type: "datetime(6)",
+                nullable: true);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "password_reset_token",
+                table: "users");
+
+            migrationBuilder.DropColumn(
+                name: "password_reset_token_expiry",
+                table: "users");
+        }
+    }
+}
